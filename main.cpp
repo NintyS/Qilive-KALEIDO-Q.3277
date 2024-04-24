96a87beec8cc197fc95630bd553f0502a28b4d86
NintySwinty <emil.panecki@gmail.com>
2024-04-24 04:12:03 +0200

    Added red light

--- main.cpp
+++ main.cpp
@@ -12,25 +12,27 @@ int main() {
   libusb_init(&ctx);
   devh = libusb_open_device_with_vid_pid(ctx, VENDOR_ID, PRODUCT_ID);
   if (!devh) {
-    fprintf(stderr, "Nie można otworzyć urządzenia.\n");
+    fprintf(stderr, "Can't open the device ( try with sudo )\n");
     return 1;
   }
 
-  // Ustaw kolor zielony
+  // Green Color
   unsigned char data[] = {0x27, 0x2B, 0x95, 0xFF, 0xE0, 0x45, 0x85, 0x6E};
+  // Red color
+  unsigned char data[] = {0x27, 0x2B, 0x95, 0x04, 0xA8, 0x45, 0x7E, 0x76};
   int transferred;
   int ret = libusb_control_transfer(
       devh, LIBUSB_REQUEST_TYPE_CLASS | LIBUSB_RECIPIENT_INTERFACE, 0x9, 0x300,
       INTERFACE, data, sizeof(data), 0);
 
   if (ret < 0) {
-    fprintf(stderr, "Błąd podczas ustawiania koloru.\n");
+    fprintf(stderr, "Error while setting color\n");
     libusb_close(devh);
     libusb_exit(ctx);
     return 1;
   }
 
-  printf("Kolor ustawiony na zielony.\n");
+  printf("Color set\n");
 
   libusb_close(devh);
   libusb_exit(ctx);
