adeae34ba641794d7266e11eb58424573877126a
NintyS <emil.panecki@gmail.com>
2024-05-18 17:48:51 +0200

    Fixed turning off leds, and added turning it on

--- main.cpp
+++ main.cpp
@@ -38,10 +38,12 @@ int set_color(libusb_device_handle *devh, libusb_context *ctx,
 // unsigned char data[] = {0x27, 0x2b, 0x9d, 0xff, 0xe0, 0x4a, 0x4d, 0x6e};
 // Purple Color ( I don't remember the values XD )
 // unsigned char data[] = {0x27, 0x26, 0x8d, 0x01, 0x88, 0x4a, 0x48, 0x86};
+// Turned off ( Leds only )
+// unsigned char data[] = {0x27, 0x2B, 0x55, 0xFF, 0xF0,0x4D, 0x7E, 0x76};
+// Turn On leds
+// unsigned char data[] = {0x27, 0x2b, 0x5d, 0xff, 0xf0, 0x4d, 0x85, 0x6e};
 
 // Works but you can't return from this even after unplugging the device
-// Turned off ( Leds only )
-// unsigned char data[] = {0x27, 002B, 0x6D, 0xFF, 0xE0, 0x4D, 0x85, 0x6E};
 // Red Breathing
 // unsigned char data[] = {0x27, 0x2B, 0x45, 0xFF, 0xF8, 0x4D, 0x85, 0x6E};
 
@@ -67,75 +69,71 @@ int main() {
     std::cout << "5. White Color\n";
     std::cout << "6. Light Blue Color\n";
     std::cout << "7. Purple Color\n";
-    std::cout << "8. Turn off leds - testing ONLY\n";
-    std::cout << "9. Red Breathing - testing ONLY\n";
-    std::cout << "10. Exit\n";
+    std::cout << "8. Turn off leds\n";
+    std::cout << "9. Turn on leds\n";
+    std::cout << "10. Red Breathing - testing ONLY\n";
+    std::cout << "11. Exit\n";
     std::cout << "=====================\n";
 
     std::cout << "Enter the color number: ";
     std::cin >> input;
 
     switch (input) {
-      case 1: {
-        unsigned char data1[] = {0x27, 0x2B, 0x95, 0xFF,
-                                 0xE0, 0x45, 0x85, 0x6E};
-        set_color(devh, ctx, data1);
-        break;
-      }
-      case 2: {
-        unsigned char data2[] = {0x27, 0x2B, 0x95, 0x04,
-                                 0xA8, 0x45, 0x7E, 0x76};
-        set_color(devh, ctx, data2);
-        break;
-      }
-      case 3: {
-        unsigned char data3[] = {0x27, 0x2B, 0x95, 0xFF,
-                                 0xE0, 0x4A, 0x46, 0x76};
-        set_color(devh, ctx, data3);
-        break;
-      }
-      case 4: {
-        unsigned char data4[] = {0x27, 0x2B, 0x9D, 0x04,
-                                 0xA8, 0x45, 0x85, 0x6E};
-        set_color(devh, ctx, data4);
-        break;
-      }
-      case 5: {
-        unsigned char data5[] = {0x27, 0x2b, 0x85, 0x04,
-                                 0xa8, 0x4a, 0x4d, 0x6e};
-        set_color(devh, ctx, data5);
-        break;
-      }
-      case 6: {
-        unsigned char data6[] = {0x27, 0x2b, 0x9d, 0xff,
-                                 0xe0, 0x4a, 0x4d, 0x6e};
-        set_color(devh, ctx, data6);
-        break;
-      }
-      case 7: {
-        unsigned char data7[] = {0x27, 0x26, 0x8d, 0x01,
-                                 0x88, 0x4a, 0x48, 0x86};
-        set_color(devh, ctx, data7);
-        break;
-      }
-      case 8: {
-        unsigned char data8[] = {0x27, 0x2B, 0x6D, 0xFF,
-                                 0xE0, 0x4D, 0x85, 0x6E};
-        set_color(devh, ctx, data8);
-        break;
-      }
-      case 9: {
-        unsigned char data9[] = {0x27, 0x2B, 0x45, 0xFF,
-                                 0xF8, 0x4D, 0x85, 0x6E};
-        set_color(devh, ctx, data9);
-        break;
-      }
-      case 10:
-        goto exit_loop;
-        break;
+    case 1: {
+      unsigned char data1[] = {0x27, 0x2B, 0x95, 0xFF, 0xE0, 0x45, 0x85, 0x6E};
+      set_color(devh, ctx, data1);
+      break;
+    }
+    case 2: {
+      unsigned char data2[] = {0x27, 0x2B, 0x95, 0x04, 0xA8, 0x45, 0x7E, 0x76};
+      set_color(devh, ctx, data2);
+      break;
+    }
+    case 3: {
+      unsigned char data3[] = {0x27, 0x2B, 0x95, 0xFF, 0xE0, 0x4A, 0x46, 0x76};
+      set_color(devh, ctx, data3);
+      break;
+    }
+    case 4: {
+      unsigned char data4[] = {0x27, 0x2B, 0x9D, 0x04, 0xA8, 0x45, 0x85, 0x6E};
+      set_color(devh, ctx, data4);
+      break;
+    }
+    case 5: {
+      unsigned char data5[] = {0x27, 0x2b, 0x85, 0x04, 0xa8, 0x4a, 0x4d, 0x6e};
+      set_color(devh, ctx, data5);
+      break;
+    }
+    case 6: {
+      unsigned char data6[] = {0x27, 0x2b, 0x9d, 0xff, 0xe0, 0x4a, 0x4d, 0x6e};
+      set_color(devh, ctx, data6);
+      break;
+    }
+    case 7: {
+      unsigned char data7[] = {0x27, 0x26, 0x8d, 0x01, 0x88, 0x4a, 0x48, 0x86};
+      set_color(devh, ctx, data7);
+      break;
+    }
+    case 8: {
+      unsigned char data8[] = {0x27, 0x2B, 0x55, 0xFF, 0xF0, 0x4D, 0x7E, 0x76};
+      set_color(devh, ctx, data8);
+      break;
+    }
+    case 9: {
+      unsigned char data8[] = {0x27, 0x2B, 0x5D, 0xFF, 0xF0, 0x4D, 0x85, 0x6E};
+      set_color(devh, ctx, data8);
+    }
+    case 10: {
+      unsigned char data9[] = {0x27, 0x2B, 0x45, 0xFF, 0xF8, 0x4D, 0x85, 0x6E};
+      set_color(devh, ctx, data9);
+      break;
+    }
+    case 11:
+      goto exit_loop;
+      break;
 
-      default:
-        std::cout << "Invalid input\n";
+    default:
+      std::cout << "Invalid input\n";
     }
     system("clear");
   }
